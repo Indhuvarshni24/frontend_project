74db0b111774bb526fc9e5e4f47881eda1b7fa32
Indhuvarshni24 <151735931+Indhuvarshni24@users.noreply.github.com>
2025-01-06 11:45:26 +0530

    Update CreateUser.jsx

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const Createuser = () => {
+const CreateUser = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
